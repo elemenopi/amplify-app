7a7284f3ecdafb77040acc847e118838a775df3b
elemenopi <lipovanilya@gmail.com>
2025-01-19 14:38:55 +0200

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Premium from './pages/Premium';
 
 function App() {
   return (
-    <div>hello</div>
+    <div className="App">
+      <Router>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/premium">Premium</NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/premium" element={<Premium />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
